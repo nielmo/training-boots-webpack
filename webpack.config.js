323b50cd594fc3086bf4c8076bc6aeb53ea4c112
Roniel Labrador <roniel.labrador@gmail.com>
2018-12-28 16:00:44 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+var WebpackNotifierPlugin = require('webpack-notifier');
+const fs = require('fs');
+
+let templates = [];
+let dir = 'src';
+let files = fs.readdirSync(dir);
+
+files.forEach(file => {
+  if (file.match(/\.pug$/)) {
+    let filename = file.substring(0, file.length - 4);
+    templates.push(
+      new HtmlWebpackPlugin({
+        template: dir + '/' + filename + '.pug',
+        filename: filename + '.html'
+      })
+    );
+  }
+});
+
+module.exports = {
+  entry: [
+    './src/js/index.js',
+    './src/scss/style.scss'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [
+          "raw-loader",
+          "pug-html-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    ...templates,
+    new HtmlWebpackPugPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./css/style.css",
+      allChunks: true
+    }),
+    new StyleLintPlugin({
+      configFile: ".stylelintrc"
+    }),
+    new WebpackNotifierPlugin()
+  ]
+}
---
+++ webpack/webpack.config.build.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var WebpackNotifierPlugin = require('webpack-notifier');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const fs = require('fs');
+
+let templates = [];
+let dir = 'src';
+let files = fs.readdirSync(dir);
+
+files.forEach(file => {
+  if (file.match(/\.pug$/)) {
+    let filename = file.substring(0, file.length - 4);
+    templates.push(
+      new HtmlWebpackPlugin({
+        template: dir + '/' + filename + '.pug',
+        filename: filename + '.html'
+      })
+    );
+  }
+});
+
+module.exports = {
+  entry: [
+    './src/js/index.js',
+    './src/scss/style.scss'
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [
+          "raw-loader",
+          "pug-html-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    ...templates,
+    new HtmlWebpackPugPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./css/style.css",
+      allChunks: true
+    }),
+    new WebpackNotifierPlugin(),
+    new CleanWebpackPlugin('dist')
+  ]
+}
